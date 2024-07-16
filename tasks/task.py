3c3a627c97ae03a01e49e592faca3129002b2d0a
Kian Hunziker <kian.hunziker@unibas.ch>
2024-07-16 12:29:24 +0200

    add task class that contain loss functions and metrics

---
+++ tasks/task.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import tasks.metrics as M
+import hydra
+import omegaconf
+
+from utils.config_utils import instantiate, to_list
+
+
+class Task:
+    def __init__(self, dataset=None, model=None, loss=None, loss_val=None, metrics=None):
+        self.dataset = dataset
+        self.model = model
+        if metrics is None: metrics = []
+        self.metric_names = to_list(metrics)
+
+        self.loss = instantiate(registry=M.metric_functions, config=loss, partial=True)
+        if loss_val is not None:
+            self.loss_val = instantiate(registry=M.metric_functions, config=loss_val, partial=True)
+        else:
+            self.loss_val = self.loss
+
+    def metrics(self, x, y,):
+        output_metrics = {
+            metric: M.metric_functions[metric](x, y)
+            for metric in self.metric_names if metric in M.metric_functions
+        }
+        return {**output_metrics}
+
+
+task_registry = {
+    'default': Task,
+    'classification': Task,
+    'regression': Task
+}
