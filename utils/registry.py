ffc8e11d6cbb34eb827345e610523f7da1f32bec
Kian Hunziker <kian.hunziker@unibas.ch>
2024-07-16 14:20:12 +0200

    add registry for optimizers, schedulers and models

---
+++ utils/registry.py
@@ -0,0 +1,23 @@
+optimizer = {
+    "adam":    "torch.optim.Adam",
+    "adamw":   "torch.optim.AdamW",
+    "rmsprop": "torch.optim.RMSprop",
+    "sgd":     "torch.optim.SGD",
+    "lamb":    "src.utils.optim.lamb.JITLamb",
+}
+
+scheduler = {
+    "constant":        "transformers.get_constant_schedule",
+    "plateau":         "torch.optim.lr_scheduler.ReduceLROnPlateau",
+    "step":            "torch.optim.lr_scheduler.StepLR",
+    "multistep":       "torch.optim.lr_scheduler.MultiStepLR",
+    "cosine":          "torch.optim.lr_scheduler.CosineAnnealingLR",
+    "constant_warmup": "transformers.get_constant_schedule_with_warmup",
+    "linear_warmup":   "transformers.get_linear_schedule_with_warmup",
+    "cosine_warmup":   "transformers.get_cosine_schedule_with_warmup",
+}
+
+model = {
+    "sashimi": "models.sashimi.sashimi_standalone.Sashimi",
+    "conv_net": "models.simple_test_models.ConvNet"
+}
