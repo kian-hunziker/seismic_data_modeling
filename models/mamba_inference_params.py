ce25b7e7eeec1727780d36dcff126d8e80aa6362
Kian Eric Hunziker <hunzik0002@sgi63.cluster.bc2.ch>
2024-08-12 11:40:04 +0200

    add inference params for evaluation of mamba models

---
+++ models/mamba_inference_params.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import Callable, Optional, Sequence, Union
+from torch import Tensor
+
+
+@dataclass
+class InferenceParams:
+    """Inference parameters that are passed to the main model in order
+    to efficienly calculate and store the context during inference."""
+
+    max_seqlen: int
+    max_batch_size: int
+    seqlen_offset: int = 0
+    batch_size_offset: int = 0
+    key_value_memory_dict: dict = field(default_factory=dict)
+    lengths_per_sample: Optional[Tensor] = None
+
+    def reset(self, max_seqlen, max_batch_size):
+        self.max_seqlen = max_seqlen
+        self.max_batch_size = max_batch_size
+        self.seqlen_offset = 0
+        if self.lengths_per_sample is not None:
+            self.lengths_per_sample.zero_()
+            
