a81e6879a90e2cf68f4431e7c888caab0d0c6654
Kian Hunziker <kian.hunziker@unibas.ch>
2024-09-24 17:17:21 +0200

    add checkpoint loading

--- simple_train.py
+++ simple_train.py
@@ -35,17 +35,18 @@ from seisbench.util import worker_seeding
 
 
 class SimpleSeqModel(pl.LightningModule):
-    def __init__(self, config):
+    def __init__(self, config, d_data: int = 3):
         super().__init__()
         self.save_hyperparameters(config)
+        self.d_data = d_data
 
         self.model = instantiate(registry.model, self.hparams.model)
         try:
             d_model = self.hparams.model.d_model
         except:
             d_model = 0
-        self.encoder = instantiate_encoder_simple(self.hparams.encoder, d_data=3, d_model=d_model)
-        self.decoder = instantiate_decoder_simple(self.hparams.decoder, d_data=3, d_model=d_model)
+        self.encoder = instantiate_encoder_simple(self.hparams.encoder, d_data=self.d_data, d_model=d_model)
+        self.decoder = instantiate_decoder_simple(self.hparams.decoder, d_data=self.d_data, d_model=d_model)
 
         self.task = instantiate(task_registry, self.hparams.task)
         self.criterion = self.task.loss
@@ -189,6 +190,60 @@ def create_trainer(config):
     return trainer
 
 
+def _extract_step_number(filename):
+    match = re.search(r'step=(\d+)\.ckpt$', filename)
+    if match:
+        return int(match.group(1))
+    return None
+
+
+def load_checkpoint(checkpoint_path: str, location: str = 'cpu', return_path: bool = False) -> tuple[
+    SimpleSeqModel, dict]:
+    """
+    Load checkpoint and hparams.yaml from specified path. Model is loaded to cpu.
+    If no checkpoint is specified, the folder is searched for checkpoints and the one with the highest
+    step number is returned.
+    :param checkpoint_path: path to checkpoint file. The hparams file is extracted automatically
+    :return: LightningSequenceModel, hparams
+    """
+    if not checkpoint_path.endswith('.ckpt'):
+        # the path does not directly lead to checkpoint, we search for checkpoints in directory
+        all_files = []
+
+        # Walk through directory and subdirectories
+        for root, dirs, files in os.walk(checkpoint_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                step_number = _extract_step_number(file)
+                if step_number is not None:
+                    all_files.append((step_number, file_path))
+        all_files.sort(key=lambda x: x[0])
+        checkpoint_path = all_files[-1][1]
+
+    hparam_path = '/'.join(checkpoint_path.split('/')[:-2]) + '/hparams.yaml'
+
+    if not os.path.isfile(checkpoint_path):
+        print('NO CHECKPOINT FOUND')
+        return None
+    if not os.path.isfile(hparam_path):
+        print('NO HPARAM FOUND')
+        hparams = None
+    else:
+        with open(hparam_path, 'r') as f:
+            hparams = yaml.safe_load(f)
+
+    print(f'Loading checkpoint from {checkpoint_path}')
+    if hparams is not None:
+        name = hparams['experiment_name']
+        print(f'Experiment name: {name}')
+
+    model = SimpleSeqModel.load_from_checkpoint(checkpoint_path, map_location=location)
+    if return_path:
+        return model, hparams, checkpoint_path
+    else:
+        return model, hparams
+
+
 @hydra.main(version_base=None, config_path="configs", config_name="config.yaml")
 def main(config: OmegaConf) -> None:
     try:
@@ -197,10 +252,10 @@ def main(config: OmegaConf) -> None:
         print(OmegaConf.to_yaml(config))
         print('*' * 32, '\n\n')
 
-        print(f'cuda available: {torch.cuda.is_available()}')
+        print(f'Cuda available: {torch.cuda.is_available()}')
 
         trainer = create_trainer(config)
-        model = SimpleSeqModel(config)
+
 
         preload = config.dataset.get('_name_') in registry.preloadable_datasets
         if preload:
@@ -209,6 +264,7 @@ def main(config: OmegaConf) -> None:
             dataset = instantiate(registry.dataset, config.dataset)
 
         if isinstance(dataset, SeisbenchDataLit):
+            print('\nInitializing Seisbench Loaders\n')
             train_loader = DataLoader(
                 dataset.dataset_train,
                 shuffle=True,
@@ -222,6 +278,7 @@ def main(config: OmegaConf) -> None:
                 **config.loader
             )
         else:
+            print('\nInitializing Standard Loaders\n')
             train_loader = DataLoader(
                 dataset.dataset_train,
                 shuffle=True,
@@ -232,6 +289,14 @@ def main(config: OmegaConf) -> None:
                 shuffle=False,
                 **config.loader
             )
+
+        d_data = dataset.d_data
+
+        if config.train.get("ckpt_path", None) is not None:
+            print(f'\nLoading checkpoint from {config.train.ckpt_path}\n')
+            model, hparams, ckpt_path = load_checkpoint(config.train.ckpt_path, return_path=True)
+        else:
+            model = SimpleSeqModel(config, d_data=d_data)
 
         summary = ModelSummary(model, max_depth=1)
         print('\n', '*' * 32, '\n')
