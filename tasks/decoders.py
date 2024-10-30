ada00ee55281ca99a5041ae55ae9d1bbbd41dd36
Kian Hunziker <kian.hunziker@unibas.ch>
2024-10-30 12:06:29 +0100

    add upsampling decoder

--- tasks/decoders.py
+++ tasks/decoders.py
@@ -410,6 +410,18 @@ class BidirPhasePickDecoder(nn.Module):
         return out
 
 
+class UpsamplingDecoder(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(UpsamplingDecoder, self).__init__()
+        self.upSample = Conv1dUpsampling(hidden_dim=in_features)
+        self.linear = nn.Linear(in_features, out_features)
+
+    def forward(self, x, state=None):
+        out = self.upSample(x)
+        out = self.linear(out)
+        return out
+
+
 dec_registry = {
     'dummy': DummyDecoder,
     'linear': LinearDecoder,
@@ -423,6 +435,7 @@ dec_registry = {
     'convnet-decoder': ConvNetDecoder,
     'bidir-autoreg-decoder': BidirAutoregDecoder,
     'bidir-phasepick-decoder': BidirPhasePickDecoder,
+    'upsampling-decoder': UpsamplingDecoder,
 }
 
 pretrain_decoders = ['transformer', 's4-decoder', 'pool', 'embedding']
