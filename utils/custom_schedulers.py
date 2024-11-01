f639bcd2fbc83c9dab8618d0d2ee16c467fe701a
Kian Hunziker <kian.hunziker@unibas.ch>
2024-11-01 11:58:56 +0100

    allow scaling of lr in second ramp

--- utils/custom_schedulers.py
+++ utils/custom_schedulers.py
@@ -2,9 +2,10 @@ import torch
 
 
 class DoubleLinearScheduler(torch.optim.lr_scheduler.LambdaLR):
-    def __init__(self, optimizer, num_warmup_steps, num_cooldown_steps, last_epoch=-1):
+    def __init__(self, optimizer, num_warmup_steps, num_cooldown_steps, lr_fraction_second_iter=1.0, last_epoch=-1):
         self.warmup_steps = num_warmup_steps
         self.cooldown_steps = num_cooldown_steps
+        self.lr_fraction_second_iter = lr_fraction_second_iter
 
         super().__init__(optimizer, self.lr_lambda, last_epoch)
 
@@ -17,7 +18,7 @@ class DoubleLinearScheduler(torch.optim.lr_scheduler.LambdaLR):
             return max(0.0, (self.warmup_steps + self.cooldown_steps - step) / self.cooldown_steps)
         # First plateau (at zero)
         elif step < 2 * self.warmup_steps + self.cooldown_steps:
-            return (step - self.warmup_steps - self.cooldown_steps) / self.warmup_steps
+            return self.lr_fraction_second_iter * (step - self.warmup_steps - self.cooldown_steps) / self.warmup_steps
 
         else:
-            return max(0.0, (2 * self.warmup_steps + 2 * self.cooldown_steps - step) / self.cooldown_steps)
+            return self.lr_fraction_second_iter * max(0.0, (2 * self.warmup_steps + 2 * self.cooldown_steps - step) / self.cooldown_steps)
